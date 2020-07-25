8087bf0be7df2b7f4b467a74d0cbb2cd5f7df167
Nurliman Diara <nurlimandiara@gmail.com>
2020-07-25 07:46:21 +0000

    add brand_collection

--- models/brand.go
+++ models/brand.go
@@ -7,10 +7,11 @@ import (
 // Brand data model
 type Brand struct {
 	gorm.Model
-	Name         string    `json:"name" gorm:"size:100;unique_index;not null" validate:"required"`
-	Description  string    `json:"description" gorm:"size:255"`
-	Products     []Product `json:"-" validate:"dive"`
-	OtherDetails string    `json:"otherDetails" gorm:"size:255"`
+	Name         string       `json:"name" gorm:"size:100;unique_index;not null" validate:"required"`
+	Description  string       `json:"description" gorm:"size:255"`
+	Products     []Product    `json:"-" validate:"dive"`
+	Collections  []Collection `json:"collections" gorm:"many2many:brand_collections;" `
+	OtherDetails string       `json:"otherDetails" gorm:"size:255"`
 }
 
 // BrandInput used when create or update data
